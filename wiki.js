039afe58f57f8df4f921e9b1d7abac511d2e4a9b
Stephen Brown <stephendbrown54@gmail.com>
2022-03-17 18:20:28 -0400

    add banner

---
+++ wiki.js
@@ -0,0 +1,13 @@
+
+document.body.insertAdjacentHTML("afterbegin", `
+    <div id='header' class='wrapper'>
+        <img src='${getRootPath()}banner.png' class='banner-image'/>
+    </div>
+`);
+document.body.insertAdjacentHTML("beforeend", `
+    <div id='footer' class='wrapper'>Footer</div>
+`);
+
+function getRootPath() {
+    return window.location.href.endsWith('index.html') ? '' : '../'
+}
